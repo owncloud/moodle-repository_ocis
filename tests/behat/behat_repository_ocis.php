ca220db618571007ec7d0b90e41f38683090ace1
Niraj Acharya <nirajacharyacode@gmail.com>
2023-11-30 15:00:35 +0545

    adding behat ui test

---
+++ tests/behat/behat_repository_ocis.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Steps definitions related to repository_ocis.
+ *
+ * @package   repository_ocis
+ * @category  test
+ * @license   GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../../lib/behat/core_behat_file_helper.php');
+
+
+/**
+ * Steps definitions related to repository_ocis.
+ */
+class behat_repository_ocis extends behat_base{}
